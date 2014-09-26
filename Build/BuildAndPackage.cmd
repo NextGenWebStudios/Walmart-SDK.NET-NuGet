256aff877a567b45d529816696c56cc37ac93eef
Jeremy Frederick <jeremy.frederick@reticentmedia.com>
2014-09-26 08:27:11 -0400

    Adding initial SDK. Implemented methods:
    GetCategories
    GetProducts
    
    Added build system from SharpArch to create NuGet package.

---
+++ Build/BuildAndPackage.cmd
@@ -0,0 +1,2 @@
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild Build.proj /t:PackageArtifacts /p:IsDesktopBuild=true
+pause
---
+++ Common/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("NextGen Web Studios")]
+[assembly: AssemblyCopyright("Copyright © NextGen Web Studios")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
